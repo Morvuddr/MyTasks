3af4d5db18a84d61b86d223ce2975aee19d12505
Igor Bopp <igor_bopp@mail.ru>
2019-12-01 11:01:40 +0300

    Initial Commit

---
+++ MyTasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTasks
+//
+//  Created by Игорь Бопп on 01.12.2019.
+//  Copyright © 2019 igor.bopp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
